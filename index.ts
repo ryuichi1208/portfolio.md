2abb1434281c18b36237ec99c45a4cc83857b1c3
Ryuichi1208 <35625782+ryuichi1208@users.noreply.github.com>
2018-11-15 09:36:10 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-<!DOCTYPE html>
+// <!DOCTYPE html>
+
+class Greeter {
+   constructor(public greeting: string) {
+    console.log("constructor");
+   }
+   greet() {
+       return this.greeting;
+   }
+};
+ 
+var greeter = new Greeter("Hello, world!");
+var str = greeter.greet();
+console.log(str);
